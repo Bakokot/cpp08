15e328dd0fc2ab5e01366cc1a35cd4e45c80fc7a
Thibault Barde-Cabucon <tbarde-c@z3r2p6.42lyon.fr>
2024-09-18 19:29:51 +0200

    Seems to be working like the test from the subject

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -14,7 +14,14 @@ class MutantStack : public std::stack<T>
 
 		MutantStack<T> operator=(const MutantStack<T> &rhs);
 
-		typedef typename ;	
+		//typedef typename std::stack<T>::container_type::iterator iterator;	
+		typedef typename std::deque<T>::iterator iterator;	
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		iterator	begin();
+		iterator	end();
+		const_iterator	cbegin() const;
+		const_iterator	cend() const;
 	private:
 
 };
@@ -45,4 +52,29 @@ MutantStack<T>	MutantStack<T>::operator=(const MutantStack<T> &rhs)
 	}
 	return (*this);
 }
+
+template<typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::begin() 
+{
+	return (this->c.begin());
+}
+
+template<typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::end() 
+{
+	return (this->c.end());
+}
+
+template<typename T>
+typename MutantStack<T>::const_iterator	MutantStack<T>::cbegin() const
+{
+	return (this->c.begin());
+}
+
+template<typename T>
+typename MutantStack<T>::const_iterator	MutantStack<T>::cend() const
+{
+	return (this->c.end());
+}
+
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,14 +1,82 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
-int main()
+void	disp(std::string str)
 {
-	MutantStack<int> stacking;
-	stacking.push(1);
-	stacking.push(5);
-	MutantStack<int> stacking2(stacking);
-	std::cout << stacking2.size() << std::endl;
-	std::cout << stacking2.top() << std::endl;
-	return (0);
+	std::cout << "-- " << str << std::endl;
 }
 
+/*
+ * My own main 
+*/
+
+/*
+ * main from the subject
+*/
+//int main()
+//{
+//	MutantStack<int> mstack;
+//	disp("Push 5 and 17");
+//	mstack.push(5);
+//	mstack.push(17);
+//	disp("Print Top");
+//	std::cout << mstack.top() << std::endl;
+//	disp("Pop");
+//	mstack.pop();
+//	disp("Print size");
+//	std::cout << mstack.size() << std::endl;
+//	disp("Push 3 - 5 - 737 - 0");
+//	mstack.push(3);
+//	mstack.push(5);
+//	mstack.push(737);
+//	////[...]
+//	mstack.push(0);
+//	MutantStack<int>::iterator it = mstack.begin();
+//	MutantStack<int>::iterator ite = mstack.end();
+//	++it;
+//	--it;
+//	disp("Print and iterate through the whole MutantStack");
+//	while (it != ite)
+//	{
+//		std::cout << *it << std::endl;
+//		++it;
+//	}
+//	std::stack<int> s(mstack);
+//	return 0;
+//}
+
+/*
+ * main from the subject in std::list version
+*/
+//int main()
+//{
+//	std::list<int> mstack;
+//	disp("Push 5 and 17");
+//	mstack.push_back(5);
+//	mstack.push_back(17);
+//	disp("Print Top");
+//	std::cout << *mstack.end() << std::endl;
+//	disp("Pop");
+//	mstack.pop_back();
+//	disp("Print size");
+//	std::cout << mstack.size() << std::endl;
+//	disp("Push 3 - 5 - 737 - 0");
+//	mstack.push_back(3);
+//	mstack.push_back(5);
+//	mstack.push_back(737);
+//	////[...]
+//	mstack.push_back(0);
+//	std::list<int>::iterator it = mstack.begin();
+//	std::list<int>::iterator ite = mstack.end();
+//	++it;
+//	--it;
+//	disp("Print and iterate through the whole std::list");
+//	while (it != ite)
+//	{
+//		std::cout << *it << std::endl;
+//		++it;
+//	}
+//	std::stack<int, std::list<int> > s(mstack);
+//	return 0;
+//}
